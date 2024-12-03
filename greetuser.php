4d89b8af343634b105d87a642de2e5b77a33c849
rajamehboob <mahboobrehman@ymail.com>
2024-12-03 22:22:13 +0500

    Create greetuser.php

---
+++ greetuser.php
@@ -0,0 +1,17 @@
+<?php
+function greetUser($name) {
+    echo "Hello, $name!\n";
+}
+
+greetUser("HAMZA");
+?>
+<?php
+// Function definition
+function greet() {
+    echo "Hello, welcome to PHP programing!\n";
+}
+
+// Function call
+greet();
+?>
+
